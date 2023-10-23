c71effa97282aada64c8db3b791087ddc5b9d9c0
LinaMernova <127698541+LinaMernova@users.noreply.github.com>
2023-10-23 15:29:38 +0300

    file.txt input class* Figures

--- 2/lab/lab/lab.cpp
+++ 2/lab/lab/lab.cpp
@@ -1,44 +1,70 @@
-﻿#include "TFigure.h"
+﻿#include <fstream>
+#include <map>
+#include <vector>
+#include "TFigure.h"
 #include "TPawn.h"
 #define MAX 32
+#define filename "figures.txt"
 
 using namespace std;
 
 int main()
 {
-	Board board;
-
-	TPawn bPawn1(black, 'a', 1);
-	TPawn wPawn2(white, 'a', 2);
-	TPawn wPawn3(white, 'a', 3);
-	TPawn wPawn4(white, 'a', 1);//!!!!!!!!!!
-	TPawn bPawn5(black, 'g', 4);
-
-	//====================
-	TFigure* figures[MAX];
-	//-Инициализация
-	for (int i = 0; i < MAX; i++)
-		figures[i] = NULL;
-	//-
-	figures[0] = &bPawn1;
-	figures[1] = &wPawn2;
-	figures[2] = &wPawn3;
-	figures[3] = &wPawn4;
-	figures[4] = &bPawn5;
-	//====================
-
-	for (int i = 0; i < MAX && figures[i] != NULL; i++)
-	{
-			figures[i]->AddFigure(board);
-			figures[i]->AddAtack(board);
+	ifstream f(filename);
+	if (!f.is_open()) {
+		cout << "ERROR " << filename;
+		return 1;
 	}
-	
-	cout << bPawn1.Count(black) << " - Black Pawn" << endl;
-	cout << bPawn1.Count(white) << " - White Pawn" << endl;
+
+	string type; string text; char x; int y;
+
+	map<string, enum Color> w_or_b;
+	w_or_b["white"] = Color::white;
+	w_or_b["black"] = Color::black;
+
+	vector<TFigure*> figures;
+	while (f >> type >> text >> x >> y)
+	{
+		TFigure* piece = nullptr;
+		Color color = w_or_b[text];
+		cout << type << " " << color << " " << x << " " << y << endl;
+		if (type == "Pawn")
+			piece = new TPawn(color, x, y);
+
+		if (piece)
+		{
+			figures.push_back(piece);
+		}
+	}
+	f.close();
+
+	Board board;
+	/*for (TFigure* piece : figures)
+	{
+		piece->AddFigure(board);
+		piece->AddAtack(board);
+	}*/
+	for (int i = 0; i < figures.size(); i++)
+	{
+		figures[i]->AddFigure(board);
+		figures[i]->AddAtack(board);
+	}
+
+	cout << figures[0]->Count(black) << " - Black Pawn" << endl;
+	cout << figures[0]->Count(white) << " - White Pawn" << endl;
 
 	board.PrintBoard();
 
 	cout << endl;
 
 	board.PrintBoardAtack();
+
+	/*for (TFigure *piece:figures)
+	{
+		delete piece;
+	}*/
+	for (int i = 0; i < figures.size(); i++)
+	{
+		delete figures[i];
+	}
 }
